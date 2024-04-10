113a52fd5f11c09c41d365f6c9c2b436f02ef810
unknown <emma.broudic35@gmail.com>
2024-04-10 14:16:14 +0200

    scripts sql découpés

---
+++ projet-code/database/schema1-ddl.sql
@@ -0,0 +1,80 @@
+CREATE TABLE "categories" (
+    id SERIAL PRIMARY KEY,
+    reference VARCHAR(10) UNIQUE NOT NULL,
+    label VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE "sizes" (
+    id SERIAL PRIMARY KEY,
+    reference VARCHAR(10) UNIQUE NOT NULL,
+    label VARCHAR(6) NOT NULL
+);
+
+CREATE TABLE "types" (
+    id SERIAL PRIMARY KEY,
+    reference VARCHAR(10) UNIQUE NOT NULL,
+    label VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE "status" (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(10),
+    description VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE "addresses" (
+    id SERIAL PRIMARY KEY,
+    road VARCHAR(150) NOT NULL,
+    zip_code CHAR(5) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    CONSTRAINT addresses_ukey UNIQUE (road, zip_code, city)
+);
+
+CREATE TABLE "users" (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(254) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    picture VARCHAR(1000),
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    created_at TIMESTAMP,
+    address_id INT,
+    FOREIGN KEY (address_id) REFERENCES addresses(id)
+);
+
+CREATE TABLE "products" (
+    id SERIAL PRIMARY KEY,
+    reference VARCHAR(20) UNIQUE NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(400),
+    picture VARCHAR(1000),
+    added_at TIMESTAMP,
+    wishlist VARCHAR(200),
+    category_id INT,
+    size_id INT,
+    type_id INT,
+    user_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (size_id) REFERENCES sizes(id),
+    FOREIGN KEY (type_id) REFERENCES types(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE "exchanges" (
+    id SERIAL PRIMARY KEY,
+    product_offered_id INT NOT NULL,
+    product_exchanged_id INT NOT NULL,
+    owner_id INT NOT NULL,
+    exchanger_id INT NOT NULL,
+    status_id INT NOT NULL,
+    CONSTRAINT exchanges_ukey UNIQUE (product_offered_id, product_exchanged_id, owner_id, exchanger_id),
+    FOREIGN KEY(product_offered_id) REFERENCES products(id),
+    FOREIGN KEY(product_exchanged_id) REFERENCES products(id),
+    FOREIGN KEY(owner_id) REFERENCES users(id),
+    FOREIGN KEY(exchanger_id) REFERENCES users(id),
+    FOREIGN KEY(status_id) REFERENCES status(id)
+);
+
+
+
---
+++ projet-code/database/schema2-data-referential.sql
@@ -0,0 +1,45 @@
+INSERT INTO categories (reference, label) VALUES
+('CAT001', 'Kids'),
+('CAT002', 'Women'),
+('CAT003', 'Men');
+
+ INSERT INTO sizes (reference, label) VALUES
+('SIZ001', 'XS'),
+('SIZ002', 'S'),
+('SIZ003', 'M'),
+('SIZ004', 'L'),
+('SIZ005', 'XL'),
+('SIZ006', '39'),
+('SIZ007', '40'),
+('SIZ008', '41'),
+('SIZ009', '42'),
+('SIZ010', '43'),
+('SIZ011', '44'),
+('SIZ012', '46'),
+('SIZ013', '38'),
+('SIZ014', '37'),
+('SIZ015', '36'),
+('SIZ016', '35'),
+('SIZ017', '34'),
+('SIZ018', '33'),
+('SIZ019', '32'),
+('SIZ020', '31');
+
+INSERT INTO types (reference, label) VALUES
+('TYP001', 'T-Shirt'),
+('TYP002', 'Pantalon'),
+('TYP003', 'Escarpins'),
+('TYP004', 'Montre'),
+('TYP005', 'Sac à dos'),
+('TYP006', 'Robe'),
+('TYP007', 'Veste'),
+('TYP008', 'Mocassins'),
+('TYP009', 'Bagages'),
+('TYP010', 'Bijoux');
+
+INSERT INTO status (code, description) VALUES
+('COD1', 'Livrer le produit à !'),
+('COD2', 'Proposition échange de'),
+('COD3', 'Echange accepté'),
+('COD4', 'Echange annulé'),
+('COD5', 'Proposition échange refusée');
